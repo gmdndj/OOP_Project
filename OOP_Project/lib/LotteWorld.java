7128f5317b8126c4d2c4aadb6370508ba2603de1
tab_bat <166480908+gmdndj@users.noreply.github.com>
2024-11-02 16:02:20 +0900

    Delete Ride in LotteWorld.java

--- OOP_Project/lib/LotteWorld.java
+++ OOP_Project/lib/LotteWorld.java
@@ -10,18 +10,4 @@ public class LotteWorld extends AmusementPark {
         super.displayInfo();
         System.out.println("Welcome to Lotte World!");
     }
-
-    public static class Rides {
-        private String rideName;
-        private String rideType;
-
-        public Rides(String rideName, String rideType) {
-            this.rideName = rideName;
-            this.rideType = rideType;
-        }
-
-        public void displayRideInfo() {
-            System.out.println("Ride Name: " + rideName + " | Ride Type: " + rideType);
-        }
-    }
 }
